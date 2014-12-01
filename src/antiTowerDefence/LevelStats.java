6612676c6fbf5a790db29f54ad5358d08e6d5e7a
Elekron <andreas.bolzyk@gmail.com>
2014-12-01 14:41:50 +0100

    levelStats

--- src/antiTowerDefence/LevelStats.java
+++ src/antiTowerDefence/LevelStats.java
@@ -2,23 +2,37 @@ package antiTowerDefence;
 
 public class LevelStats {
 
-    public Level(Terrain[][], LevelStats)
- // 1. Sätter map och stats
- // 2. Initierar towers och units
- Terrain[][] map
- Tower[] towers
- Unit[] units
- LevelStats stats
- Position startPos
- addUnit(Unit unit):void
- setTowers(Tower[]):void
- updateMap():void 
- // Efter units och towers har gjort
- sina “actions”, så går den i genom towers och units och kollar om torn eller gubbar tillkommit.
- alterTerrain(Terrain t):void 
- // Lägger till exempelvis en Portal på en väg
- getUnits():Unit[]
- getTowers():Tower[]
+    LevelStats(int winScore, int credits){
 
-    
+    }
+    // Sätter score & credits
+    // Sätter score = 0
+    private int score;
+    private int winScore;
+    private int credits;
+    private int level;
+
+    // Eventuella variabler för antal döda units, köpta units och units som nått målet
+    public void addScore(int score){
+	
+    }
+
+    public int getScore(){
+	return score;
+    }
+
+    public void addCredits(int score){
+
+    }
+
+    public int getCredits(){
+	return credits;
+    }
+
+    public int getWinScore(){
+	return winScore;
+    }
+
+
+
 }
