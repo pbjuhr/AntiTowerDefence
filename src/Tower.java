3e609ee5519d618df00378802e43e706b9b2aec0
Elekron <andreas.bolzyk@gmail.com>
2014-12-01 14:23:12 +0100

    fel

--- src/Tower.java
+++ src/Tower.java
@@ -1,4 +1,19 @@
+import unit.Unit;
+
 
 public class Tower {
 
+	private Position position;
+	private int range;
+	private int damage;
+	private int coolDown;
+	
+	public Unit[] inRange(){
+		return null;
+		
+	}
+	
+	public void shoot(Unit[] unit){
+		
+	}
 }
