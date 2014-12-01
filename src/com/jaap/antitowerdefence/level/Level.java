5f82dc95c073f08fd4a50b35f4751bfa3134a98c
Anngud <osterlund.anna@gmail.com>
2014-12-01 15:05:17 +0100

    Level
    Möjligt att getTowers metoden är överflödig.

--- src/com/jaap/antitowerdefence/level/Level.java
+++ src/com/jaap/antitowerdefence/level/Level.java
@@ -1,5 +1,47 @@
 package com.jaap.antitowerdefence.level;
 
+import antiTowerDefence.Position;
+import antiTowerDefence.Tower;
+import terrain.Terrain;
+import unit.Unit;
+
 public class Level {
 
+    private Terrain[][] map;
+    private Tower[] towers;
+    private Unit[] units;
+    private LevelStats levelStats;
+    private Position startPosition;
+    
+    public Level(Terrain[][] map, LevelStats levelStats) {
+	// 1. Sätter map och stats
+	// 2. Initierar towers och units
+    }
+    
+    public void addUnit(Unit unit) {
+	
+    }
+    
+    public void setTowers(Tower[] towers) {
+	
+    }
+    
+    public void updateMap() {
+	/* Efter units och towers har gjort sina “actions”, så går den i genom 
+	 * towers och units och kollar om torn eller gubbar tillkommit.
+	 */
+    }
+    
+    public void alterTerrain(Terrain t, Position position) {
+	// Lägger till exempelvis en Portal på en väg
+    }
+    
+    public Unit[] getUnits() {
+	return units;
+    }
+    
+    //ONÖDIG FUNKTION??
+    public Tower[] getTowers() {
+	return towers;
+    }
 }
