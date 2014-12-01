a80156ffb39cd6a94cabd58e93f2234ec5519504
pbjuhr <bjuhr.peter@gmail.com>
2014-12-01 14:54:33 +0100

    fixed LevelStuff

--- src/com/jaap/antitowerdefence/level/LevelStats.java
+++ src/com/jaap/antitowerdefence/level/LevelStats.java
@@ -2,37 +2,36 @@ package com.jaap.antitowerdefence.level;
 
 public class LevelStats {
 
-    LevelStats(int winScore, int credits){
-
-    }
-    // Sätter score & credits
-    // Sätter score = 0
     private int score;
     private int winScore;
     private int credits;
     private int level;
 
-    // Eventuella variabler för antal döda units, köpta units och units som nått målet
-    public void addScore(int score){
+    LevelStats(int winScore, int credits) {
+	// Sätter score & credits
+	// Sätter score = 0
+    }
+
+    // Eventuella variabler för antal döda units, köpta units och units som nått
+    // målet
+    public void addScore(int score) {
 
     }
 
-    public int getScore(){
+    public int getScore() {
 	return score;
     }
 
-    public void addCredits(int score){
+    public void addCredits(int score) {
 
     }
 
-    public int getCredits(){
+    public int getCredits() {
 	return credits;
     }
 
-    public int getWinScore(){
+    public int getWinScore() {
 	return winScore;
     }
-
-
 
 }
