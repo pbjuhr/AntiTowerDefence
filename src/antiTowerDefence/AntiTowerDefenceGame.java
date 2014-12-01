733b95662ea2939eea5bdb2f890c9e2b263ffb8e
pbjuhr <bjuhr.peter@gmail.com>
2014-12-01 15:26:43 +0100

    Game updates
    Added methods and constructor

--- src/antiTowerDefence/AntiTowerDefenceGame.java
+++ src/antiTowerDefence/AntiTowerDefenceGame.java
@@ -1,19 +1,67 @@
 package antiTowerDefence;
 
+import java.util.ArrayList;
+
 import com.jaap.antitowerdefence.level.Level;
 import com.jaap.antitowerdefence.level.LevelReader;
 
 public class AntiTowerDefenceGame {
-    
-    private int minTimeStep;
+
+    private int minTimeStep = 1000; // 1 second
     private int currentStep;
-    private int currentLevel;
+    private int currentLevelNumber;
     private LevelReader theLevelReader;
-    private Level theLevel;
+    private Level currentLevel;
     private TowerPlacerAI towerPlacer;
-    
+    private HighScoreDB highScore;
+
     public AntiTowerDefenceGame(String level){
+	currentStep = 0;
+	currentLevelNumber = 1;
+	theLevelReader = new LevelReader(level);
+	highScore = new HighScoreDB();
+	currentLevel = new Level(theLevelReader.getMap(), theLevelReader.getPossibleTowerPositions());
+	//towerPlacer = new TowerPlacerAI(theLevelReader.get);
+    }
+
+    public void newLevel() {
+
+    }
+
+    public void step() {
+	/* Kör tidssteg!
+	0. Om x tidssteg: Flytta torn (och sätt cooldown)
+	1. Om ej cooldown: Tornen ska skjuta
+	2. Gubbarna som blir skjutna tar skada
+	3. Gubbar som inte har hälsa kvar dör
+	4. Gubbarna ska gå (anropa också LandOn)
+	5. Kontrollera och uppdatera status i gubbar 
+	6. Statistik ska uppdateras
+	*/
+    }
+    
+    public void updateStats() {
 	
     }
     
+    public boolean hasWon() {
+	return false;
+    }
+    
+    public boolean hasLost() {
+	return false;
+    }
+    
+    public boolean isHighScore() {
+	return false;
+    }
+    
+    public void setHighScore() {
+	
+    }
+    
+    public ArrayList<String> getHighScore() {
+	return highScore.getScores();
+    }
+
 }
