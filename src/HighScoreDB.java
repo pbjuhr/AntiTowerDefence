97323848f53150051730678d3dba4c9aaf5f1524
Elekron <andreas.bolzyk@gmail.com>
2014-12-01 14:22:45 +0100

    add class

--- src/HighScoreDB.java
+++ src/HighScoreDB.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
 
 public class HighScoreDB {
 
+	public void addScore(int points, int levels, String name){
+		
+	}
+	
+	public void removeScores() {
+		
+	}
+	
+	public ArrayList<String> getScores(){
+		return null;
+		
+	}
+	
 }
