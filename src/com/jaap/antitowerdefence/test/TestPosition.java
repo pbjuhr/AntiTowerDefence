83282012b8da56b3b47b84110d909d634b0daacb
pbjuhr <bjuhr.peter@gmail.com>
2014-12-03 14:16:53 +0100

    Test package

---
+++ src/com/jaap/antitowerdefence/test/TestPosition.java
@@ -0,0 +1,29 @@
+/**
+ * Example test class to test Position
+ * @author Peter Bjuhr
+ */
+
+package com.jaap.antitowerdefence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPosition {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
