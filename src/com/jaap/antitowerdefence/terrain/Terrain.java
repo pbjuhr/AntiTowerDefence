efb5db3466ea567f59d1479581452d5a79d84b6d
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 19:00:49 +0100

    Class terrain complete
    Abstract class terrain is now complete.
    Alla class that extends terrain get error missing constructor.

--- src/com/jaap/antitowerdefence/terrain/Terrain.java
+++ src/com/jaap/antitowerdefence/terrain/Terrain.java
@@ -1,11 +1,35 @@
 package com.jaap.antitowerdefence.terrain;
 
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 import com.jaap.antitowerdefence.antiTowerDefence.Position;
-
 
 public abstract class Terrain {
     private Position position;
     private boolean buildable;
     private boolean walkable;
-    
+
+    public Terrain(Position position, boolean buildable, boolean walkable) {
+	this.position = position;
+	this.buildable = buildable;
+	this.walkable = walkable;
+    }
+
+    public Position getPosition() {
+	return position;
+    }
+
+    public boolean isBuildable() {
+	return buildable;
+    }
+
+    public boolean isWalkable() {
+	return walkable;
+    }
+
 }
+
+
