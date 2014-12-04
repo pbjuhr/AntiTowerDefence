b07a6f36fb8068fc2bb3941e3cc4ed75a08774bf
Anngud <osterlund.anna@gmail.com>
2014-12-04 13:28:34 +0100

    Position implemented
    Constructor, getters and setters

--- src/com/jaap/antitowerdefence/antiTowerDefence/Position.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/Position.java
@@ -1,11 +1,32 @@
 package com.jaap.antitowerdefence.antiTowerDefence;
-
+/**
+ * 
+ * @author Anna Osterlund, id10aod
+ *
+ */
 public class Position {
 
     private int x;
     private int y;
 
-    public Position() {
-
+    public Position(int x, int y) {
+	this.x = x;
+	this.y = y;
+    }
+    
+    public int getX() {
+	return x;
+    }
+    
+    public int getY() {
+	return y;
+    }
+    
+    public void setX(int x) {
+	this.x = x;
+    }
+    
+    public void setY(int y) {
+	this.y = y;
     }
 }
