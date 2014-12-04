120f0dc326d8efad404e858c51b70ba65b9a2c84
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:07:44 +0100

    Goal add method
    Add constructor to Goal

--- src/com/jaap/antitowerdefence/terrain/Goal.java
+++ src/com/jaap/antitowerdefence/terrain/Goal.java
@@ -1,5 +1,17 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Goal extends Terrain implements LandOn {
+
+    public Goal(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
+	// TODO Auto-generated constructor stub
+    }
 
 }
