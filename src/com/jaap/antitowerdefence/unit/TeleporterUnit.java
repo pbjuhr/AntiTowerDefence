d7f03b4c41d100de80a284353d43a47250a7694e
pbjuhr <bjuhr.peter@gmail.com>
2014-12-04 16:31:56 +0100

    Teleporter class commented

--- src/com/jaap/antitowerdefence/unit/TeleporterUnit.java
+++ src/com/jaap/antitowerdefence/unit/TeleporterUnit.java
@@ -1,21 +1,52 @@
+/**
+ * @author Peter Bjuhr
+ * Class represents a teleporter unit
+ */
+
 package com.jaap.antitowerdefence.unit;
 
 import com.jaap.antitowerdefence.terrain.Portal;
 
-public class TeleporterUnit extends Unit{
+public class TeleporterUnit extends Unit {
     
+    /* Variables related to the portal */
     private Portal thePortal;
+    private int placedPortals;
     
-    public TeleporterUnit(){
+    /**
+     * Runs super constructor and sets teleporters speed, cost, health and
+     * portal variables
+     */
+    public TeleporterUnit() {
 	super();
 	speed = 20;
 	cost = 200;
 	health = 70;
 	thePortal = new Portal();
+	placedPortals = 0;
     }
     
-    public void placePortal(){
-	
+    /**
+     * Places a portal on a given position
+     */
+    public void placePortal() {
+	placedPortals++;
     }
     
+    /**
+     * Gets the teleporters portal
+     * @return thePortal
+     */
+    public Portal getPortal() {
+	return thePortal;
+    }
+    
+    /**
+     * Gets the number of placed portals
+     * @return placedPortals, nr of placed portals
+     */
+    public int getPlacedPortals() {
+	return placedPortals;
+    }
+
 }
