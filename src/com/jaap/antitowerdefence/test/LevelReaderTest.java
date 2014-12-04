53f66003c7de5a931046588ee2ac86fa6b936f3d
Anngud <osterlund.anna@gmail.com>
2014-12-04 11:40:07 +0100

    Created a new levelreadertest

--- src/com/jaap/antitowerdefence/test/LevelReaderTest.java
+++ src/com/jaap/antitowerdefence/test/LevelReaderTest.java
@@ -4,21 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.jaap.antitowerdefence.level.LevelReader;
+import com.jaap.antitowerdefence.level.LevelStats;
+
 public class LevelReaderTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+    static LevelReader levelReader;
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+	levelReader = new LevelReader("src/levels.xml");
+	
     }
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void testLevelReader() {
+	assertNotNull(levelReader);
+    }
+
+    @Test
+    public void testGetLevelStats() {
+	LevelStats levelstats= levelReader.getLevelStats(1);
+	System.out.println("credits = "+levelstats.getCredits());
+	System.out.println("score = "+levelstats.getScore());
+	System.out.println("winScore = "+levelstats.getWinScore());
+	assertNotNull(levelstats);
+    }
+
+    @Test
+    public void testGetNrOfLevels() {
+	int nrLev = levelReader.getNrOfLevels();
+	System.out.println(nrLev);
+	assertEquals(nrLev, 2);
     }
 
 }
