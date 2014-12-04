e010904998cb776a5211c7d6a04821d22a9893aa
Anngud <osterlund.anna@gmail.com>
2014-12-04 11:41:16 +0100

    LevelStatsTest
    Initiated levelstatstest

---
+++ src/com/jaap/antitowerdefence/test/LevelStatsTest.java
@@ -0,0 +1,44 @@
+package com.jaap.antitowerdefence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LevelStatsTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Test
+    public void testLevelStats() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddScore() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetScore() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddCredits() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCredits() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetWinScore() {
+	fail("Not yet implemented");
+    }
+
+}
