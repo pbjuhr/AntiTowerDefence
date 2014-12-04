2f6e5d885bfeaed68dc8689c21e5b3d870c9bc7e
pbjuhr <bjuhr.peter@gmail.com>
2014-12-04 16:34:41 +0100

    Commented Unit Classes

--- src/com/jaap/antitowerdefence/unit/FarmerUnit.java
+++ src/com/jaap/antitowerdefence/unit/FarmerUnit.java
@@ -1,7 +1,14 @@
+/**
+ * @author Peter Bjuhr
+ * Class represents a farmer unit
+ */
 package com.jaap.antitowerdefence.unit;
 
 public class FarmerUnit extends Unit {
-
+    
+    /**
+     * Runs super constructor and sets speed, cost and health
+     */
     public FarmerUnit() {
 	super();
 	speed = 20;
--- src/com/jaap/antitowerdefence/unit/SoldierUnit.java
+++ src/com/jaap/antitowerdefence/unit/SoldierUnit.java
@@ -1,7 +1,15 @@
+/**
+ * @author Peter Bjuhr
+ * Class represents a soldier unit
+ */
+
 package com.jaap.antitowerdefence.unit;
 
 public class SoldierUnit extends Unit{
     
+    /**
+     * Runs super constructor and sets speed, cost and health
+     */
     public SoldierUnit(){
 	super();
 	speed = 20;
