009061719066cd00093fe1986621d820db31b144
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:02:08 +0100

    Road add method
    Add constructor to road

--- src/com/jaap/antitowerdefence/terrain/Road.java
+++ src/com/jaap/antitowerdefence/terrain/Road.java
@@ -1,5 +1,27 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Road extends Terrain {
 
+    public Road(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
+    }
+
+    @Override
+    public boolean isWalkable() {
+	// TODO Auto-generated method stub
+	return super.isWalkable();
+    }
+
+    @Override
+    public boolean isBuildable() {
+	// TODO Auto-generated method stub
+	return super.isBuildable();
+    }
 }
