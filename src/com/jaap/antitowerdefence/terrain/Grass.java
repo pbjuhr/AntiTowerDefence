0e213261847cee8bd71914fb35772265fcdd7a67
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:07:28 +0100

    Grass add method
    Add constructor to Grass

--- src/com/jaap/antitowerdefence/terrain/Grass.java
+++ src/com/jaap/antitowerdefence/terrain/Grass.java
@@ -1,5 +1,16 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Grass extends Terrain {
+
+    public Grass(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
+    }
 
 }
