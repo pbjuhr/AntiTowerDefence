07f78841ce47dee3f5d99675cf95cabccdc82c40
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:05:11 +0100

    Start add method
    Add constructor to Start

--- src/com/jaap/antitowerdefence/terrain/Start.java
+++ src/com/jaap/antitowerdefence/terrain/Start.java
@@ -1,5 +1,16 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Start extends Terrain {
+
+    public Start(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
+    }
 
 }
