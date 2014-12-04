ab07cee45cb9cc75f3596c552bbbca6ff37d0144
Anngud <osterlund.anna@gmail.com>
2014-12-04 10:35:42 +0100

    LevelStats almost done
    All functionallity except for the calculation of credits recieved when a
    unit reaches goal. maybe that should be done outside of levelstats and
    passed as a parameter?

--- src/com/jaap/antitowerdefence/level/LevelStats.java
+++ src/com/jaap/antitowerdefence/level/LevelStats.java
@@ -1,21 +1,25 @@
 package com.jaap.antitowerdefence.level;
-
+/**
+ * 
+ * @author Anna Osterlund, id10aod
+ *
+ */
 public class LevelStats {
 
     private int score;
     private int winScore;
     private int credits;
-    private int level;
 
     LevelStats(int winScore, int credits) {
-	// S√§tter score & credits
-	// S√§tter score = 0
+	this.winScore = winScore;
+	this.credits = credits;
+	score = 0;
     }
 
     // Eventuella variabler f√∂r antal d√∂da units, k√∂pta units och units som n√•tt
     // m√•let
     public void addScore(int score) {
-
+	this.score += score;
     }
 
     public int getScore() {
@@ -23,7 +27,9 @@ public class LevelStats {
     }
 
     public void addCredits(int score) {
-
+	//Hur r‰knas credits?
+	//Det h‰r ‰r bara tillf‰lligt fˆr test.
+	credits += (score*10);
     }
 
     public int getCredits() {
