1a0dc7d55829f1a992323c786bcf8b5a0a75313b
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:06:37 +0100

    Water add method
    Add constructor to Water

--- src/com/jaap/antitowerdefence/terrain/Water.java
+++ src/com/jaap/antitowerdefence/terrain/Water.java
@@ -1,8 +1,15 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Water extends Terrain {
 
-    public Water() {
-	// This water works for Peter
+    public Water(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
     }
 }
