af6d25794065f1d364bf217d19f4815325b603cc
pbjuhr <bjuhr.peter@gmail.com>
2014-12-04 16:16:29 +0100

    Added Portal to teleporter unit

--- src/com/jaap/antitowerdefence/unit/TeleporterUnit.java
+++ src/com/jaap/antitowerdefence/unit/TeleporterUnit.java
@@ -1,11 +1,21 @@
 package com.jaap.antitowerdefence.unit;
 
+import com.jaap.antitowerdefence.terrain.Portal;
+
 public class TeleporterUnit extends Unit{
+    
+    private Portal thePortal;
     
     public TeleporterUnit(){
 	super();
 	speed = 20;
 	cost = 200;
 	health = 70;
+	thePortal = new Portal();
     }
+    
+    public void placePortal(){
+	
+    }
+    
 }
