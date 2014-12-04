2d10abc62ff01cde59e1bd2367408023e606b211
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:07:06 +0100

    Portal add method
    Add constructor to Portal

--- src/com/jaap/antitowerdefence/terrain/Portal.java
+++ src/com/jaap/antitowerdefence/terrain/Portal.java
@@ -1,5 +1,16 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Portal extends Terrain implements LandOn {
+
+    public Portal(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
+    }
 
 }
