9f56d93518334e02cd9955e63858e5db1b62bf14
Elekron <andreas.bolzyk@gmail.com>
2014-12-04 20:05:51 +0100

    Switch add method
    Add constructor to Switch

--- src/com/jaap/antitowerdefence/terrain/Switch.java
+++ src/com/jaap/antitowerdefence/terrain/Switch.java
@@ -1,5 +1,16 @@
 package com.jaap.antitowerdefence.terrain;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+
+/**
+ * 
+ * @author Andreas Bolzyk id10abk
+ *
+ */
 public class Switch extends Terrain implements LandOn{
+
+    public Switch(Position position, boolean buildable, boolean walkable) {
+	super(position, buildable, walkable);
+    }
 
 }
