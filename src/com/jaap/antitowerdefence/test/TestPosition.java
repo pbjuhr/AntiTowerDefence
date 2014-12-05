5cf9f188e1ae0275373a99430bab7004d9959799
Anngud <osterlund.anna@gmail.com>
2014-12-05 13:47:47 +0100

    testPosition done
    Test for every method in position and comments

--- src/com/jaap/antitowerdefence/test/TestPosition.java
+++ src/com/jaap/antitowerdefence/test/TestPosition.java
@@ -5,25 +5,77 @@
 
 package com.jaap.antitowerdefence.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+/**
+ * TestPosition.java
+ * 
+ * Creates an instance of Position and tests the functionallity
+ * 
+ * @author Anna Osterlund, id10aod
+ *
+ */
 public class TestPosition {
+    Position position;
+    int x;
+    int y;
 
+    /**
+     * setUp() initiates x and y and creates an instance of Position
+     */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+	x = 4;
+	y = 6;
+	position = new Position(x, y);
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
+    /**
+     * testSetX() checks if it is possible to set the value of 
+     * x in the Position
+     */
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void testSetX() {
+	int x;
+	this.x = 5;
+	position.setX(this.x);
+	x = position.getX();
+	assertEquals(this.x, x);
+    }
+
+    /**
+     * testSetY() checks if it is possible to set the value of 
+     * y in the Position
+     */
+    @Test
+    public void testSetY() {
+	int y;
+	this.y = 5;
+	position.setY(this.y);
+	y = position.getY();
+	assertEquals(this.y, y);
+    }
+
+    /**
+     * testGetX() checks if Position returns the correct value of x
+     */
+    @Test
+    public void testGetX() {
+	int x = position.getX();
+	assertEquals(this.x, x);
+    }
+
+    /**
+     * testGetY() checks if Position returns the correct value of y
+     */
+    @Test
+    public void testGetY() {
+	int y = position.getY();
+	assertEquals(this.y, y);
     }
 
 }
