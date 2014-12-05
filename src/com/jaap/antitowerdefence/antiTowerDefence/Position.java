80f142952039ba6aec23e2048a559cbc3ad7160f
Anngud <osterlund.anna@gmail.com>
2014-12-05 14:02:54 +0100

    Position comments done

--- src/com/jaap/antitowerdefence/antiTowerDefence/Position.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/Position.java
@@ -1,5 +1,8 @@
 package com.jaap.antitowerdefence.antiTowerDefence;
 /**
+ * Position.java
+ * 
+ * A class that contains x and y coordinates for a position in a 2-dim matrix
  * 
  * @author Anna Osterlund, id10aod
  *
@@ -9,23 +12,45 @@ public class Position {
     private int x;
     private int y;
 
+    /**
+     * Constructor that is used when a Position is initiated. Sets the values
+     * of the coordinates
+     * @param x - the x-coordinate
+     * @param y - the y-coordinate
+     */
     public Position(int x, int y) {
 	this.x = x;
 	this.y = y;
     }
     
+    /**
+     * getX provides the x-coordinate of the position
+     * @return the x-coordinate
+     */
     public int getX() {
 	return x;
     }
     
+    /**
+     * getY provides the y-coordinate of the position
+     * @return the y-coordinate
+     */
     public int getY() {
 	return y;
     }
     
+    /**
+     * setX sets the x-coordinate of the position
+     * @param x - is set as x-coordinate of the position
+     */
     public void setX(int x) {
 	this.x = x;
     }
     
+    /**
+     * setY sets the y-coordinate of the position
+     * @param y - is set as y-coordinate of the position
+     */
     public void setY(int y) {
 	this.y = y;
     }
