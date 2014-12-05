e2c4595766d6ef056553bdc4387999848f8410fb
Anngud <osterlund.anna@gmail.com>
2014-12-05 14:33:22 +0100

    LevelStatsTest comments

--- src/com/jaap/antitowerdefence/test/LevelStatsTest.java
+++ src/com/jaap/antitowerdefence/test/LevelStatsTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 
 import com.jaap.antitowerdefence.level.LevelStats;
 /**
+ * LevelStatsTest.java
+ * 
+ * Tests the functionality of the class LevelStats.java
  * 
  * @author Anna Österlund, id10aod
  *
@@ -15,16 +18,27 @@ import com.jaap.antitowerdefence.level.LevelStats;
 public class LevelStatsTest {
 
     private LevelStats levelstats;
+    
+    /**
+     * A new Object of LevelStats is initiated before each test method
+     */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 	levelstats = new LevelStats(60, 500);
     }
 
+    /**
+     * Test that checks if the LevelStats object was created.
+     */
     @Test
     public void testLevelStats() {
 	assertNotNull(levelstats);
     }
 
+    /**
+     * testAddScore adds some points to the score and checks if the score
+     * has increased
+     */
     @Test
     public void testAddScore() {
 	int oldScore;
@@ -35,12 +49,19 @@ public class LevelStatsTest {
 	assertTrue((newScore - oldScore) == 30);
     }
 
+    /**
+     * Checks if getScore provides the correct score
+     */
     @Test
     public void testGetScore() {
 	levelstats.addScore(30);
 	assertTrue((levelstats.getScore()) == 30);
     }
 
+    /**
+     * testAddCredits adds a value to the credits and checks if the credits
+     * has increased
+     */
     @Test
     public void testAddCredits() {
 	int oldCredits;
@@ -48,16 +69,20 @@ public class LevelStatsTest {
 	oldCredits = levelstats.getCredits();
 	levelstats.addCredits(20);
 	newCredits = levelstats.getCredits();
-	System.out.println("Old = "+oldCredits);
-	System.out.println("New = "+newCredits);
 	assertTrue(newCredits > oldCredits);
     }
 
+    /**
+     * Checks if getCredits provides the correct credit value
+     */
     @Test
     public void testGetCredits() {
 	assertTrue(levelstats.getCredits() == 500);
     }
 
+    /**
+     * Checks if getWinScore provides the correct winscore
+     */
     @Test
     public void testGetWinScore() {
 	assertTrue(levelstats.getWinScore() == 60);
