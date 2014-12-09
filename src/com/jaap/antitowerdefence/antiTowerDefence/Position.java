210382aff12fe2d3991e816bac9750222518b597
Anngud <osterlund.anna@gmail.com>
2014-12-09 07:10:40 +0100

    Position - getPosTo**** methods added
    Functions to get neightbours to the position (north, south, west, east)

--- src/com/jaap/antitowerdefence/antiTowerDefence/Position.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/Position.java
@@ -9,8 +9,8 @@ package com.jaap.antitowerdefence.antiTowerDefence;
  */
 public class Position {
 
-    private int x;
-    private int y;
+    private int x;	//The x-coorinate of the position
+    private int y;	//The y-coorinate of the position
 
     /**
      * Constructor that is used when a Position is initiated. Sets the values
@@ -22,7 +22,7 @@ public class Position {
 	this.x = x;
 	this.y = y;
     }
-    
+
     /**
      * getX provides the x-coordinate of the position
      * @return the x-coordinate
@@ -30,7 +30,7 @@ public class Position {
     public int getX() {
 	return x;
     }
-    
+
     /**
      * getY provides the y-coordinate of the position
      * @return the y-coordinate
@@ -38,7 +38,7 @@ public class Position {
     public int getY() {
 	return y;
     }
-    
+
     /**
      * setX sets the x-coordinate of the position
      * @param x - is set as x-coordinate of the position
@@ -46,7 +46,7 @@ public class Position {
     public void setX(int x) {
 	this.x = x;
     }
-    
+
     /**
      * setY sets the y-coordinate of the position
      * @param y - is set as y-coordinate of the position
@@ -54,7 +54,7 @@ public class Position {
     public void setY(int y) {
 	this.y = y;
     }
-    
+
     /**
      * distanceTo calculates the Manhattan/taxi cab distance between two
      * positions
@@ -64,7 +64,43 @@ public class Position {
     public int distanceTo(Position position) {
 	// (avstånd = |x1 - x2| + |y1 - y2|)
 	int distance = ((Math.abs(position.getX() - x) + 
-				(Math.abs(position.getY() - y))));
+		(Math.abs(position.getY() - y))));
 	return distance;
+    }
+
+    /**
+     * getPosToNorth provides the neighbour position north of this position
+     * @return - Position to north
+     */
+    public Position getPosToNorth() {
+	Position north = new Position(x, (y - 1));
+	return north;
+    }
+
+    /**
+     * getPosToSouth provides the neighbour position south of this position
+     * @return - Position to south
+     */
+    public Position getPosToSouth() {
+	Position south = new Position(x, (y + 1));
+	return south;
+    }
+
+    /**
+     * getPosToWest provides the neighbour position west of this position
+     * @return - Position to west
+     */
+    public Position getPosToWest() {
+	Position west = new Position((x - 1), y);
+	return west;
+    }
+
+    /**
+     * getPosToEast provides the neighbour position east of this position
+     * @return - Position to east
+     */
+    public Position getPosToEast() {
+	Position east = new Position((x + 1), y);
+	return east;
     }
 }
