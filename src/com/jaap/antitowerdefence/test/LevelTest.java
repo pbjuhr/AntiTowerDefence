252122d6a9b96b23f8fba5e692039fa8476b45e3
Anngud <osterlund.anna@gmail.com>
2014-12-09 15:12:07 +0100

    LevelTest - init

---
+++ src/com/jaap/antitowerdefence/test/LevelTest.java
@@ -0,0 +1,63 @@
+package com.jaap.antitowerdefence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jaap.antitowerdefence.level.Level;
+
+public class LevelTest {
+
+    Level level;
+    @BeforeClass
+    public static void setUpBeforeClass() {
+	
+    }
+
+    @Test
+    public void testLevel() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetWalkableTerrain() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPossibleTowerPositions() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddUnit() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetUnits() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetTowers() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTowers() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetLevelStats() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdateUnits() {
+	fail("Not yet implemented");
+    }
+
+}
