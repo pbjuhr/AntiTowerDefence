9617b6231ef8b6c198f3a161a4bd1d25a4f6f80d
Elekron <andreas.bolzyk@gmail.com>
2014-12-09 13:43:55 +0100

    fix commit error

--- src/com/jaap/antitowerdefence/terrain/Portal.java
+++ src/com/jaap/antitowerdefence/terrain/Portal.java
@@ -10,6 +10,7 @@ import com.jaap.antitowerdefence.unit.Unit;
  */
 public class Portal extends Terrain implements LandOnInterface {
 
+    Portal endPortal;
     // Position position
     private Position reciever;
     // boolean hasTwin
@@ -32,14 +33,31 @@ public class Portal extends Terrain implements LandOnInterface {
 
     // setTwinPosition(Position pos):void
     // setHasTwin(boolean hasTwin):void
+//<<<<<<< Updated upstream
     public void setReciever(Position p) {
 	this.reciever = p;
 	this.hasReciever = true;
+//=======
+//    public void setPosEndPortal(Position posEndPortal) {
+//	endPortal = new Portal(posEndPortal);
+//	this.posEndPortal = posEndPortal;
+//	hasEndPortal = true;
+//>>>>>>> Stashed changes
     }
 
     // getTwinPosition():Position
     public Position getReciever() {
 	return this.reciever;
+    }
+
+
+    /*
+     * TODO Need a interface method her to check if unit is on me. Need to now
+     * if unit is on my position
+     */
+    // Test of random
+    public static void main(String[] args) {
+	
     }
 
 }
