84d1dbf7c2a4be3de72ac5c7133058830765014d
Anngud <osterlund.anna@gmail.com>
2014-12-09 11:58:35 +0100

    Connection

--- src/com/jaap/antitowerdefence/antiTowerDefence/HighScoreDB.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/HighScoreDB.java
@@ -1,22 +1,45 @@
 package com.jaap.antitowerdefence.antiTowerDefence;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Properties;
 
-
+/**
+ * 
+ * @author Anna Osterlund, id10aod
+ *
+ */
 public class HighScoreDB {
-
+    Connection conn;
+    
     public HighScoreDB() {
-	
+	connectToDB();
+    }
+
+    private void connectToDB() {
+	Properties connectionProps = new Properties();
+	connectionProps.put("user", "root");
+	connectionProps.put("password", "Anngurt_8580");
+	try {
+	    conn = DriverManager.getConnection(
+	            "jdbc:mysql://localhost:80/atd_high_score",
+	            connectionProps);
+	} catch (SQLException e) {
+	    // TODO Om uppkoppling misslyckas
+	    e.printStackTrace();
+	}
     }
     
-    public void addScore(int points, int levels, String name){
+    public void addScore(String name, int score) {
 
     }
-
+/*
     public void removeScores() {
 
     }
-
-    public ArrayList<String> getScores(){
+*/
+    public ArrayList<String> getScores() {
 	return null;
 
     }
