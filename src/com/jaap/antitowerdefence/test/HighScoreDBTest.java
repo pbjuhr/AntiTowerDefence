63cfcdd7769f1974184632af4672f8d65136918a
Anngud <osterlund.anna@gmail.com>
2014-12-10 10:29:48 +0100

    HighScoreDBTest complete with comments

--- src/com/jaap/antitowerdefence/test/HighScoreDBTest.java
+++ src/com/jaap/antitowerdefence/test/HighScoreDBTest.java
@@ -6,18 +6,60 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jaap.antitowerdefence.antiTowerDefence.HighScoreDB;
-
+/**
+ * HighScoreDBTest.java
+ * 
+ * Tests the functionality of the class HighScoreDB.java
+ * 
+ * @author Anna Osterlund, id10aod
+ *
+ */
 public class HighScoreDBTest {
 
     private static HighScoreDB highScore;
+    
+    /**
+     * Initiates a HighScoreDB before running the tests
+     */
     @BeforeClass
     public static void setUpBeforeClass(){
 	highScore = new HighScoreDB();
     }
 
+    /**
+     * Tests if highScore was initiated
+     */
     @Test
     public void testHighScoreDB() {
 	assertNotNull(highScore);
     }
-
+    
+    /**
+     * Tests the addScore method
+     */
+    @Test
+    public void testAddScore() {
+	String result;
+	result = highScore.addScore("Frans", 19);
+	assertEquals(result, "success");
+    }
+    
+    /**
+     * Tests the getHighScoreTopTen method
+     */
+    @Test 
+    public void testGetHighScoreTopTen() {
+	String[][] result;
+	result = highScore.getHighScoreTopTen();
+	assertNotNull(result);
+    }
+    
+    /**
+     * Tests the isHighScore method
+     */
+    @Test
+    public void testIsHighScore() {
+	String result = highScore.isHighScore(6);
+	assertTrue(result != "fail");
+    }
 }
