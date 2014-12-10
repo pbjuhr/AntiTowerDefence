e14b26bf950f3db34a5110ea6e05819285fbb379
Elekron <andreas.bolzyk@gmail.com>
2014-12-10 16:06:53 +0100

    Goal Test

---
+++ src/com/jaap/antitowerdefence/test/GoalTest.java
@@ -0,0 +1,35 @@
+package com.jaap.antitowerdefence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+import com.jaap.antitowerdefence.terrain.Goal;
+
+public class GoalTest {
+
+    private static Goal g;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+	g = new Goal(new Position(1, 1));
+    }
+
+    @Test
+    public void testBuildable() {
+	assertFalse(g.isBuildable());
+    }
+
+    @Test
+    public void testWalkable() {
+	assertTrue(g.isWalkable());
+    }
+
+    @Test
+    public void testPosition() {
+	assertTrue(new Position(1, 1).equals(g.getPosition()));
+    }
+
+}
