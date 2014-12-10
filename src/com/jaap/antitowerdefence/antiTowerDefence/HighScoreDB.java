8b2a2993f7e12742e57a0f4c82a895feea892248
Anngud <osterlund.anna@gmail.com>
2014-12-10 11:39:07 +0100

    HighScoreDB - some comments

--- src/com/jaap/antitowerdefence/antiTowerDefence/HighScoreDB.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/HighScoreDB.java
@@ -7,19 +7,32 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 /**
+ * HighScoreDB.java
+ * 
+ * TODO: BESKRIVNING, om uppkoppling till databasen misslyckas?
  * 
  * @author Anna Osterlund, id10aod
  *
  */
 public class HighScoreDB {
+    
+    
+    /*Variables*/
     Connection conn;
     PreparedStatement addHighScore;
     PreparedStatement getHighScore;
+    
+    /**
+     * 
+     */
     public HighScoreDB() {
 	connectToDB();
 	setPreparedStatments();
     }
 
+    /**
+     * connectToDB
+     */
     private void connectToDB() {
 	Properties connectionProps = new Properties();
 	connectionProps.put("user", "root");
@@ -42,9 +55,19 @@ public class HighScoreDB {
 	}
     }
 
+    /**
+     * addScore checks the name for invalid characters and if the name is valid,
+     * adds the score to the database.
+     * @param name - player name
+     * @param score - player score
+     * @return success if the score was added, invalid if name containd invalid
+     * characters, fail if the database insertion failed
+     */
     public String addScore(String name, int score) {
-	//Kolla följande?
-	/*; ' -- xp_*/
+	if(name.contains(";") || name.contains("'") || name.contains("--") || 
+		name.contains("xp_") || name.contains("/*")){
+	    return "invalid";
+	}
 	try {
 	    addHighScore.setString(1, name);
 	    addHighScore.setInt(2, score);
@@ -55,6 +78,11 @@ public class HighScoreDB {
 	return "success";
     }
 
+    /**
+     * getHighScoreTopTen retrives the ten highest scores from the database, 
+     * saves the result(name, score) in a String matrix and returns the result
+     * @return A String matrix with the result or null
+     */
     public String[][] getHighScoreTopTen() {
 	ResultSet topTen;
 	int i = 0;
@@ -67,30 +95,43 @@ public class HighScoreDB {
 		i++;
 	    }
 	} catch (SQLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
 	    return null;
 	}
 	return highScore;
     }
     
+    /**
+     * isHighScore checks if a given score is a valid highscore, top ten or 
+     * higher
+     * @param score - The score to check
+     * @return true if the score qualifies, false is the score does not
+     * qualify, fail if the communication with the database fails
+     */
     public String isHighScore(int score) {
-	ResultSet topTen;	
+	ResultSet topTen;
+	int i = 0;
 	try {
 	    topTen = getHighScore.executeQuery();
 	    while (topTen.next()) {
 		if(score > topTen.getInt("Score")){
 		    return "true";
 		}
+		i++;
 	    }
 	} catch (SQLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
 	    return "fail";
+	}
+	if(i < 10){
+	    return "true";
 	}
 	 return "false";
     }
 
+    /**
+     * setPreparedStatements constructs prepared SQL-statements for inserting
+     * a highscore into the database and for retrieveing the ten highest scores
+     * from the database.
+     */
     private void setPreparedStatments() {
 	try {
 	    addHighScore = conn.prepareStatement(
