9d1f4a8649dc621dc73fffdc2ca565d15ce4337d
Elekron <andreas.bolzyk@gmail.com>
2014-12-10 16:27:48 +0100

    Start test

---
+++ src/com/jaap/antitowerdefence/test/StartTest.java
@@ -0,0 +1,35 @@
+package com.jaap.antitowerdefence.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
+import com.jaap.antitowerdefence.terrain.Start;
+
+public class StartTest {
+
+    private static Start s;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+	s = new Start(new Position(1, 1));
+    }
+
+    @Test
+    public void testBuildable() {
+	assertFalse(s.isBuildable());
+    }
+
+    @Test
+    public void testWalkable() {
+	assertTrue(s.isWalkable());
+    }
+
+    @Test
+    public void testPosition() {
+	assertTrue(new Position(1, 1).equals(s.getPosition()));
+    }
+
+}
