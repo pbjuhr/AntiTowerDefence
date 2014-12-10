8098ec6f8c099e1c7b2014a8a618d1009f7529ac
Elekron <andreas.bolzyk@gmail.com>
2014-12-10 15:44:31 +0100

    Road test fix

--- src/com/jaap/antitowerdefence/unit/RoadTest.java
+++ src/com/jaap/antitowerdefence/unit/RoadTest.java
@@ -18,28 +18,25 @@ import com.jaap.antitowerdefence.terrain.Road;
 public class RoadTest {
 
     private static Road r;
-    
+
     @BeforeClass
-    public static void setUpBeforeClass(){
-	r = new Road(new Position(1,1));
+    public static void setUpBeforeClass() {
+	r = new Road(new Position(1, 1));
     }
-    
-    
-    
+
     @Test
     public void testBuildable() {
 	assertFalse(r.isBuildable());
     }
-    
+
     @Test
     public void testWalkable() {
 	assertTrue(r.isWalkable());
     }
-    
 
     @Test
     public void testPosition() {
-	assertTrue(new Position(1,1).equals(r.getPosition()));
+	assertTrue(new Position(1, 1).equals(r.getPosition()));
     }
 
 }
