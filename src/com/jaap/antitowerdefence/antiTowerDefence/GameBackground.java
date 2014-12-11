d569791f1ab841c701f2e3cc6affcda4a570bfaf
Eldskegg <joakim.sandman@gmail.com>
2014-12-11 21:12:29 +0100

    Essentially done game panels

--- src/com/jaap/antitowerdefence/antiTowerDefence/GameBackground.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/GameBackground.java
@@ -8,60 +8,67 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
-import com.jaap.antitowerdefence.terrain.Grass;
 import com.jaap.antitowerdefence.terrain.Terrain;
-
+//TODO errorhandling
 public class GameBackground extends JComponent {
 
     private static final long serialVersionUID = 1L;
     private Terrain[] grass;
     private Terrain[] road;
 
-    public GameBackground() {//Terrain[] grass, Terrain[] road) {
-//	this.grass = grass;
-//	this.road = road;
-	grass = new Terrain[1];
-	grass[0] = new Grass(new Position(1, 2));
+    public GameBackground() {
+	repaint();
+    }
+
+    public void setTerrain(Terrain[] grass, Terrain[] road) {
+	this.grass = grass;
+	this.road = road;
 	repaint();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        drawWater(g);
-
-	for (Terrain t : grass) {
-	    BufferedImage grass = null;
-	    try {
-		grass = ImageIO.read(new File("assets/img/" + t.getClass().getSimpleName() + ".png"));
-	    } catch (IOException e) {
-		//e.printStackTrace();
-	    }
-	    g.drawImage(grass, t.getPosition().getX()*32, t.getPosition().getY()*32, null);
-	}
-
-//	for (Terrain t : road) {
-//	    BufferedImage road = null;
-//	    try {
-//		road = ImageIO.read(new File(getClass().getResource("../../../../../assets/img/" + t.getClass().getSimpleName() + ".png").getFile()));
-//	    } catch (IOException e) {
-//		//e.printStackTrace();
-//	    }
-//	    g.drawImage(road, t.getPosition().getX()*32, t.getPosition().getY()*32, null);
-//	}
+	super.paintComponent(g);
+	drawWater(g);
+	drawGrass(g);
+	drawRoad(g);
     }
 
     private void drawWater(Graphics g) {
-	BufferedImage water = null;
+	BufferedImage waterImg = null;
 	try {
-	    water = ImageIO.read(new File("assets/img/Water.png"));
+	    waterImg = ImageIO.read(new File("assets/img/Water.png"));
 	} catch (IOException e) {
 	    //e.printStackTrace();
 	}
 	for (int row = 0; row < 15; row++) {
 	    for (int col = 0; col < 20; col++) {
-		g.drawImage(water, col*32, row*32, null);
+		g.drawImage(waterImg, col*32, row*32, null);
 	    }
+	}
+    }
+
+    private void drawGrass(Graphics g) {
+	for (Terrain t : grass) {
+	    BufferedImage grassImg = null;
+	    try {
+		grassImg = ImageIO.read(new File("assets/img/Grass.png"));
+	    } catch (IOException e) {
+		//e.printStackTrace();
+	    }
+	    g.drawImage(grassImg, t.getPosition().getX()*32, t.getPosition().getY()*32, null);
+	}
+    }
+
+    private void drawRoad(Graphics g) {
+	for (Terrain t : road) {
+	    BufferedImage roadImg = null;
+	    try {
+		roadImg = ImageIO.read(new File("assets/img/Road.png"));
+	    } catch (IOException e) {
+		//e.printStackTrace();
+	    }
+	    g.drawImage(roadImg, t.getPosition().getX()*32, t.getPosition().getY()*32, null);
 	}
     }
 
--- src/com/jaap/antitowerdefence/antiTowerDefence/GameForeground.java
+++ src/com/jaap/antitowerdefence/antiTowerDefence/GameForeground.java
@@ -6,74 +6,139 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.Timer;
 
-//import com.jaap.antitowerdefence.terrain.Terrain;
-//import com.jaap.antitowerdefence.unit.FarmerUnit;
-//import com.jaap.antitowerdefence.unit.Unit;
-
+import com.jaap.antitowerdefence.terrain.Portal;
+import com.jaap.antitowerdefence.terrain.Switch;
+import com.jaap.antitowerdefence.terrain.Terrain;
+import com.jaap.antitowerdefence.unit.Unit;
+//TODO errorhandling switchlisteners working towers remove testunit
 public class GameForeground extends JComponent {
 
     private static final long serialVersionUID = 1L;
-    //private Unit[] units;
-//    private Tower[] towers;
+    private ArrayList<Unit> units;
+    private Tower[] towers;
+    private Terrain[] road;
     //private Terrain[] switches;
-int dx=0;
-    public GameForeground() {//Unit[] units, Tower[] towers, Terrain[] switches) { // portals
-//	towers = new Tower[1];
-//	towers[0] = new Tower();
-//	towers[0].setPosition(new Position(5, 10));
-	Timer timer = new Timer(50, new ActionListener() {
+    int animationOffset;
+    double realOffset;
+
+    public GameForeground(int fps) {
+	animationOffset = 0;
+	realOffset = 0;
+	Timer timer = new Timer(Math.round(1000/fps), new ActionListener() {
 	    @Override
 	    public void actionPerformed(ActionEvent arg0) {
 		repaint();
-		dx++;
+		realOffset = (realOffset + 32.0/fps) % 32;
+		animationOffset = (int) realOffset;
+		if (!units.isEmpty()) {
+		    units.get(0).setPosition(new Position(5, 10));
+		    units.get(0).setDirection(Direction.EAST);
+		}
 	    }
 	});
 	timer.setRepeats(true);
 	timer.start();
-//	repaint();
+    }
+
+    public void setTerrainAndObjects(ArrayList<Unit> units, Tower[] towers, Terrain[] road) {
+	this.units = units;
+	this.towers = towers;
+	this.road = road;
+	repaint();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+	super.paintComponent(g);
+	drawRoadObjects(g);
+	drawTowers(g);
+	drawUnits(g);
+    }
 
-//	for (Unit u : units) {
-//	    BufferedImage unit = null;
+    private void drawRoadObjects(Graphics g) {
+	for (Terrain t : road) {
+	    BufferedImage roadObjectImg = null;
+	    try {
+		switch (t.getClass().getSimpleName()) {
+		case "Start":
+		    roadObjectImg = ImageIO.read(new File("assets/img/Start.png"));
+		    break;
+		case "Goal":
+		    roadObjectImg = ImageIO.read(new File("assets/img/Goal.png"));
+		    break;
+		case "Portal":
+		    if (((Portal) t).hasReciever()) {
+			roadObjectImg = ImageIO.read(new File("assets/img/Portal_start.png"));
+		    } else {
+			roadObjectImg = ImageIO.read(new File("assets/img/Portal_end.png"));
+		    }
+		    break;
+		case "Switch":
+		    switch (((Switch) t).getDirection()) {
+		    case EAST:
+			roadObjectImg = ImageIO.read(new File("assets/img/DirBig/EAST.png"));
+			break;
+		    case NORTH:
+			roadObjectImg = ImageIO.read(new File("assets/img/DirBig/NORTH.png"));
+			break;
+		    case SOUTH:
+			roadObjectImg = ImageIO.read(new File("assets/img/DirBig/SOUTH.png"));
+			break;
+		    case WEST:
+			roadObjectImg = ImageIO.read(new File("assets/img/DirBig/WEST.png"));
+			break;
+		    }
+		    break;
+		}
+		g.drawImage(roadObjectImg, t.getPosition().getX()*32, t.getPosition().getY()*32, null);
+	    }
+	    catch (IOException e) {
+		//e.printStackTrace();
+	    }
+	}
+    }
+
+    private void drawTowers(Graphics g) {
+//	for (Tower t : towers) {
+//	    BufferedImage towerImg = null;
 //	    try {
-////		unit = ImageIO.read(new File(getClass().getResource("../../../../../assets/img/" + u.getClass().getSimpleName() + ".png").getFile()));
-//		unit = ImageIO.read(new File(getClass().getResource("../../../../../assets/img/Farmer.png").getFile()));
+//		towerImg = ImageIO.read(new File("assets/img/" + t.getClass().getSimpleName() + ".png"));
 //	    } catch (IOException e) {
 //		//e.printStackTrace();
 //	    }
-//	    g.drawImage(unit, u.getPosition().getX()*32, u.getPosition().getY()*32, null);
+//	    g.drawImage(towerImg, t.getPosition().getX()*32, t.getPosition().getY()*32, null);
 //	}
+    }
 
-//	for (Tower t : towers) {
-	    BufferedImage tower = null;
+    private void drawUnits(Graphics g) {
+	for (Unit u : units) {
+	    BufferedImage unitImg = null;
 	    try {
-//		tower = ImageIO.read(new File(getClass().getResource("../../../../../assets/img/" + t.getClass().getSimpleName() + ".png").getFile()));
-		tower = ImageIO.read(new File("assets/img/Tower.png"));
+		unitImg = ImageIO.read(new File("assets/img/" + u.getClass().getSimpleName() + ".png"));
 	    } catch (IOException e) {
 		//e.printStackTrace();
 	    }
-	    g.drawImage(tower,5*32+dx, 10*32-dx, null);
-//	}
-
-//	for (Terrain s : switches) {
-//	    BufferedImage switchPlate = null;
-//	    try {
-////		switchPlate = ImageIO.read(new File(getClass().getResource("../../../../../assets/img/" + s.getClass().getSimpleName() + ".png").getFile()));
-//		switchPlate = ImageIO.read(new File(getClass().getResource("../../../../../assets/img/Portal.png").getFile()));
-//	    } catch (IOException e) {
-//		//e.printStackTrace();
-//	    }
-//	    g.drawImage(switchPlate, s.getPosition().getX()*32, s.getPosition().getY()*32, null);
-//	}
+	    switch (u.getDirection()) {
+	    case EAST:
+		g.drawImage(unitImg, u.getPosition().getX()*32 + animationOffset, u.getPosition().getY()*32, null);
+		break;
+	    case NORTH:
+		g.drawImage(unitImg, u.getPosition().getX()*32, u.getPosition().getY()*32 - animationOffset, null);
+		break;
+	    case SOUTH:
+		g.drawImage(unitImg, u.getPosition().getX()*32, u.getPosition().getY()*32 + animationOffset, null);
+		break;
+	    case WEST:
+		g.drawImage(unitImg, u.getPosition().getX()*32 - animationOffset, u.getPosition().getY()*32, null);
+		break;
+	    }
+	}
     }
 
 }
