4bfbf009da0a2879a04ff7320591bd7b12521de7
Anngud <osterlund.anna@gmail.com>
2014-12-11 14:42:08 +0100

    Level - method to set the switches added

--- src/com/jaap/antitowerdefence/level/Level.java
+++ src/com/jaap/antitowerdefence/level/Level.java
@@ -2,7 +2,11 @@ package com.jaap.antitowerdefence.level;
 
 import java.util.ArrayList;
 
+import com.jaap.antitowerdefence.antiTowerDefence.Direction;
+import com.jaap.antitowerdefence.antiTowerDefence.Position;
 import com.jaap.antitowerdefence.antiTowerDefence.Tower;
+import com.jaap.antitowerdefence.terrain.Start;
+import com.jaap.antitowerdefence.terrain.Switch;
 import com.jaap.antitowerdefence.terrain.Terrain;
 import com.jaap.antitowerdefence.unit.Unit;
 /**
@@ -17,12 +21,12 @@ import com.jaap.antitowerdefence.unit.Unit;
 public class Level {
 
     /*Variables*/
-    private Terrain[] walkable;		//Terrain elements units can walk on
-    private Terrain[] buildable;	//Possible tower position
+    private Terrain[] road;		//Terrain elements units can walk on
+    private Terrain[] grass;	//Possible tower position
     private Tower[] towers;		//Towers implemented in level
     private ArrayList<Unit> units;	//Units currently active in game
     private LevelStats levelStats;	//Level info and game statistics
-    
+
     /**
      * Contructor for Level saves the walkable terrain for the units, the 
      * terrain where towers can be built and the level info of the level. The 
@@ -33,30 +37,31 @@ public class Level {
      * @param levelStats - Level info and game statistics
      */
     public Level(Terrain[] walkable, Terrain[] buildable, 
-	    				LevelStats levelStats) {
-	this.walkable = walkable;
-	this.buildable = buildable;
+	    LevelStats levelStats) {
+	this.road = walkable;
+	this.grass = buildable;
 	this.levelStats = levelStats;
+	setSwitches();
 	units = new ArrayList<Unit>();
     }
-    
+
     /**
      * getWalkableTerrain provides the walkable terrain elements for the units
      * @return - Terrain array of Road
      */
     public Terrain[] getWalkableTerrain() {
-	return walkable;
+	return road;
     }
-    
+
     /**
      * getPossibleTowerPositions provides the terrain elements where towers 
      * can be built
      * @return - array of possible tower build positions
      */
     public Terrain[] getPossibleTowerPositions() {
-	return buildable;
+	return grass;
     }
-    
+
     /**
      * addUnit adds a unit to the ArrayList of units. The cost of the Unit is
      * decrease from the players credit 
@@ -66,7 +71,7 @@ public class Level {
 	units.add(unit);	
 	levelStats.decreaseCredits(unit.getCost());
     }
-    
+
     /**
      * getUnits provides the ArrayList of active units
      * @return
@@ -74,7 +79,7 @@ public class Level {
     public ArrayList<Unit> getUnits() {
 	return units;
     }
-    
+
     /**
      * setTowers replaces the tower array with a new array of towers and
      * gives the new towers access to the ArrayList of units
@@ -86,7 +91,7 @@ public class Level {
 	    towers[i].setUnits(units);
 	}
     }
-    
+
     /**
      * getTowers provides the towers in the level
      * @return - array of towers in level
@@ -94,7 +99,7 @@ public class Level {
     public Tower[] getTowers() {
 	return towers;
     }
-    
+
     /**
      * getLevelStats provides the levelStats instance of the level
      * @return - the LevelStats object of the level
@@ -102,7 +107,7 @@ public class Level {
     public LevelStats getLevelStats() {
 	return levelStats;
     }
-    
+
     /**
      * updateUnits checks if any of the units has reached goal or died. If a
      * units has died it is removed from the list of active units. If a unit
@@ -134,5 +139,55 @@ public class Level {
 	int score = (int)Math.round((unit.getHealth()/10));
 	levelStats.addCredits(totalCredits);
 	levelStats.addScore(score);
+    }
+
+    /**
+     * setSwitches loops through the road array and checks for intersections.
+     * If an intersection is found a road switch is created and added to the
+     * Terrain array of road. The switch is informed about which directions it
+     * can redirect units.
+     */
+    private void setSwitches() {
+	int neighbours;
+	ArrayList<Direction> directions;
+	for(int i = 0; i < road.length; i++) {
+	    directions = new ArrayList<Direction>();
+	    neighbours = 0;
+	    if(roadContains(road[i].getPosition().getPosToEast())) {
+		directions.add(Direction.EAST);
+		neighbours++;
+	    }
+	    if(roadContains(road[i].getPosition().getPosToSouth())) {
+		directions.add(Direction.SOUTH);
+		neighbours++;
+	    }
+	    if(roadContains(road[i].getPosition().getPosToNorth())) {
+		directions.add(Direction.NORTH);
+	    }
+	    if(roadContains(road[i].getPosition().getPosToWest())) {
+		directions.add(Direction.WEST);
+	    }
+	    
+	    if(road[i] instanceof Start){
+		road[i].setSwitch(directions);
+	    }else if(neighbours > 1){
+		road[i] = new Switch(road[i].getPosition(), directions);		
+	    }
+	}
+    }
+
+    /**
+     * roadPosition check if a Terrain element of Road exists on the given
+     * position
+     * @param roadPosition - The position to check
+     * @return true if there is road on the position, false if not.
+     */
+    private boolean roadContains(Position roadPosition) {
+
+	for(Terrain t : road) {
+	    if(t.getPosition().equals(roadPosition));
+	    return true;
+	}
+	return false;
     }
 }
